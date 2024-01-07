6660d491fb4648cb7b1f0c3fbfe698d5b57de27a
Prapti Jaduvanshi <khushi.jaduvanshi@gmail.com>
2024-01-07 06:15:53 +0530

    Tested Button component, added next-auth authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import Button from "@/components/ui/Button"
 import { db } from "@/lib/db"
 
 export default async function Home() {
 
-  return <div className='text-red-500'>Hello World</div>
+  return <Button> Hello </Button>
 }
