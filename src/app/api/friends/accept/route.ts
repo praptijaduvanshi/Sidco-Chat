25322410c24e891935020e550d457f5e18d4492f
Prapti Jaduvanshi <khushi.jaduvanshi@gmail.com>
2024-01-27 22:17:47 +0530

    Error handling: accepted friend in realtime

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from '@/helpers/redis'
 import { authOptions } from '@/lib/auth'
 import { db } from '@/lib/db'
+import { pusherServer } from '@/lib/pusher'
+import { toPusherKey } from '@/lib/utils'
 import { getServerSession } from 'next-auth'
 import { z } from 'zod'
 
@@ -49,9 +51,18 @@ export async function POST(req: Request) {
     const user = JSON.parse(userRaw) as User
     const friend = JSON.parse(friendRaw) as User
 
-    // notify added user
-
+    //Notify to added user
     await Promise.all([
+      pusherServer.trigger(
+        toPusherKey(`user:${idToAdd}:friends`),
+        'new_friend',
+        user
+      ),
+      pusherServer.trigger(
+        toPusherKey(`user:${session.user.id}:friends`),
+        'new_friend',
+        friend
+      ),
       //Add to the friends set, 
       db.sadd(`user:${session.user.id}:friends`, idToAdd),
       //Add friend the other way around as well
